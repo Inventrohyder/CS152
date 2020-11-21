077210ba1447a7c3cab641f091899e5d2df6ad22
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-11-21 03:19:40 +0100

    ✨ feat: update Literal with utility functions

--- assignment-3/literal.py
+++ assignment-3/literal.py
@@ -4,9 +4,26 @@ class Literal:
         self.name = name
         self.sign = True  # Initialize the literal sign as positive
 
-    def __neg__(self) -> None:
+    def pure(self):
+        """Gets the pure version of the Literal without any negative"""
+        if self.sign:
+            return self
+        else:
+            return -self
+
+    def __neg__(self):
         """Flip the sign of the Literal"""
         l = Literal(self.name)
         l.sign = not self.sign
         return l
-        
+
+    def __repr__(self) -> str:
+        sign = "" if self.sign else "-"
+        return f"{sign}{self.name}"
+
+    def __eq__(self, o: object) -> bool:
+        """Two literals are equal when their names are equal"""
+        return self.name == o.name
+
+    def __hash__(self) -> int:
+        return self.name.__hash__()
